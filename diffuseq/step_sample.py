028ed0777cb6eee8594570db4a971e5df4cfbf79
klxu03 <beaver2025@gmail.com>
2024-05-07 11:03:17 -0400

    removed dist utils from step sample

--- diffuseq/step_sample.py
+++ diffuseq/step_sample.py
@@ -96,12 +96,8 @@ class LossAwareSampler(ScheduleSampler):
         """
         batch_sizes = [
             th.tensor([0], dtype=th.int32, device=local_ts.device)
-            for _ in range(dist.get_world_size())
+            for _ in range(1)
         ]
-        dist.all_gather(
-            batch_sizes,
-            th.tensor([len(local_ts)], dtype=th.int32, device=local_ts.device),
-        )
 
         # Pad all_gather batches to be the maximum batch size.
         batch_sizes = [x.item() for x in batch_sizes]
@@ -109,8 +105,6 @@ class LossAwareSampler(ScheduleSampler):
 
         timestep_batches = [th.zeros(max_bs).to(local_ts) for bs in batch_sizes]
         loss_batches = [th.zeros(max_bs).to(local_losses) for bs in batch_sizes]
-        dist.all_gather(timestep_batches, local_ts)
-        dist.all_gather(loss_batches, local_losses)
         timesteps = [
             x.item() for y, bs in zip(timestep_batches, batch_sizes) for x in y[:bs]
         ]
