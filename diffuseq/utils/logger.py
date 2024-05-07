1bafc8de32371a5e8196d960666ad929a4ea6ead
klxu03 <beaver2025@gmail.com>
2024-05-07 11:03:41 -0400

    removed logger dist utils

--- diffuseq/utils/logger.py
+++ diffuseq/utils/logger.py
@@ -368,11 +368,10 @@ class Logger(object):
                 d["dummy"] = 1  # so we don't get a warning about empty dict
         # LISA
         out = d.copy()  # Return the dict for unit testing purposes
-        if int(os.environ['LOCAL_RANK']) == 0:
-            wandb.log({**d})
-            for fmt in self.output_formats:
-                if isinstance(fmt, KVWriter):
-                    fmt.writekvs(d)
+        wandb.log({**d})
+        for fmt in self.output_formats:
+            if isinstance(fmt, KVWriter):
+                fmt.writekvs(d)
         self.name2val.clear()
         self.name2cnt.clear()
         return out
